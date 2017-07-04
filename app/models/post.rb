ef6d929c3b4e35936111d8d71c8038b8e49479f9
Tyler Wright <samoasao@gmail.com>
2017-07-04 18:06:38 +0000

    Add simple form

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   validates :image, presence: true
   
   has_attached_file :image, styles: { :medium => "640px" }
-  validates_attachement_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
