7d4bec7bf593521613b1b1e12a6aae4294f419f7
Tyler Wright <samoasao@gmail.com>
2017-07-05 00:05:12 +0000

    Form Partial

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  
+  before_action :set_post, only: [:show, :edit, :update, :destroy]  
+  
   def index
     @posts = Post.all.reverse
     
@@ -15,21 +18,17 @@ class PostsController < ApplicationController
   end
   
   def show
-    @post = Post.find(params[:id])
   end
   
   def edit  
-    @post = Post.find(params[:id])
   end
   
-  def update  
-    @post = Post.find(params[:id])
+  def update
     @post.update(post_params)
     redirect_to(post_path(@post))
   end  
   
   def destroy  
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
   end  
@@ -40,6 +39,10 @@ class PostsController < ApplicationController
   def post_params  
     params.require(:post).permit(:image, :caption)
   end  
+  
+  def set_post
+    @post = Post.find(params[:id])
+  end
   
   
   
