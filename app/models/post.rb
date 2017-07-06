a26dd25e38a52f61ea0a5c35f8880f9d39c65f34
Tyler Wright <samoasao@gmail.com>
2017-07-06 05:29:35 +0000

    Add User functionality

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   validates :image, presence: true
-  
+  validates :user_id, presence: true  
   has_attached_file :image, styles: { :medium => "640" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  
+  belongs_to :user
 end
