5213f60ae380ac95aefb595d1196a8f5ef75ee2c
Tyler Wright <samoasao@gmail.com>
2017-07-10 00:22:22 +0000

    Fix caption width

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170706184101) do
+ActiveRecord::Schema.define(version: 20170710001545) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20170706184101) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.text     "caption"
+    t.string   "caption"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.string   "image_file_name"
